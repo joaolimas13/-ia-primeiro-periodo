b7b51d1ab880cf96ff70001b25d1dffafd2ef0f5
Joao Pedro <joaolimas13@gmail.com>
2026-01-11 21:54:38 -0300

    Ajustes nos exercícios (Dia 2/3)

--- dia-02/exercicios.py
+++ dia-02/exercicios.py
@@ -3,5 +3,27 @@ n2 = input("Digite o segundo número: ")
 print("Somando do jeito errado:", n1 + n2)
 
 a = float(n1)
-b = float(n2)2
+b = float(n2)
 print("Somando do jeito certo:", a + b)
+
+nome = input ("qual seu nome?")
+idade = input ("qual sua idade?")
+peso = input ("qual seu peso?")
+print (nome,idade,peso)
+
+
+nome = input ("qual seu nome?")
+print ("seja bem vindo, {nome}")
+
+
+num1 = input("escolha 1 numero:")
+num2 = input("escolha outro numero:")
+soma = int(num1) + int(num2)
+
+print("a soma é" ,soma)
+
+nome = input("qual seu nome?")
+print ("seja bem vindo",nome)
+
+print("tipo de nome:", type(nome))
+print("quantidade  de letras:",len(nome))
